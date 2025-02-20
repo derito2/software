4152c11c69987597f1909e7bd21719f5a2da05f0
Diego Rdz <a00835032@tec.mx>
2025-02-19 22:38:02 -0600

    Page 2

---
+++ src/app/page2/page.tsx
@@ -0,0 +1,8 @@
+export default function Page2() {
+    return (
+      <div className="flex flex-col items-center justify-center text-center h-screen">
+        <h1 className="text-4xl font-bold">In Process</h1>
+      </div>
+    );
+  }
+  

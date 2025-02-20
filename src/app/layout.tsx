6019b57cf58792a40fcf34c112d72fc2ca5063c5
Diego Rdz <a00835032@tec.mx>
2025-02-19 22:18:00 -0600

    Home Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
 import Link from 'next/link';
-import '../styles/globals.css';
+import './globals.css';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <nav className="bg-gray-800 p-4 text-white flex justify-between">
-        <Link href="/" className="text-xl font-bold">LOGO</Link>
-        <div>
-          <Link href="/" className="mx-2">Home</Link>
-          <Link href="/page1" className="mx-2">Pag 1</Link>
-          <Link href="/page2" className="mx-2">Pag 2</Link>
-        </div>
-      </nav>
-      <main className="p-4">{children}</main>
-    </div>
+    <html lang="es">
+      <head>
+        <title>Mi Sitio</title>
+      </head>
+      <body className="flex flex-col items-center justify-center min-h-screen">
+        <nav className="bg-gray-800 p-4 text-white flex justify-center w-full">
+          <div className="flex space-x-8">
+            <Link href="/" className="text-xl font-bold ">LOGO</Link>
+            <Link href="/" className="mx-2">Home</Link>
+            <Link href="/page1" className="mx-2">Pag 1</Link>
+            <Link href="/page2" className="mx-2">Pag 2</Link>
+          </div>
+        </nav>
+        <main className="p-4 flex items-center justify-center flex-grow w-full">{children}</main>
+      </body>
+    </html>
   );
 }
